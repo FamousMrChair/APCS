42f998affc0e37e067214976d17f766baa36d1c1
FamousMrChair <kxiao30@stuy.edu>
2021-09-30 12:28:13 -0400

    init setup

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,11 @@
+public class Greet {
+	public static void main(String[] args){
+		greet("Jomin");
+		greet("Anthony");
+		greet("Kevin");
+	}
+	public static void greet(String str){
+		System.out.println("Why, hello there, " + str + ". How do you do?");
+	}
+}
+
