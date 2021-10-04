f95b872ee198bc95b754450ee945caf90811db0b
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-10-03 20:37:53 -0400

    Update Greet.java

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,9 @@
+import BigSib;
+public class Greet{
+	public static void main(String[] args){
+        bigsib.greet("Jomin");
+		    bigsib.greet("Anthony");
+		    bigsib.greet("Kevin");
+	}
+}
+    
