40cc411a378ffce69d11ab2f27ba4f64f170999d
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-10-05 22:30:31 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -13,7 +13,10 @@ How to import all of the classes of a java file?
 
 public class BigSib {
 	String HelloMsg;
+	public static void HelloMsg(String str){
+		HelloMsg = greets;
+	}
 	public static String greet(String str){
-		return HelloMsg + str;
+		return HelloMsg + " " + str;
 	}
 }
