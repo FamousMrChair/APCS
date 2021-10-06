bfe4266a952caf326fbe0ef6dccb001586d390c5
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-10-05 21:50:09 -0400

    Update BigSib.java

--- 10_bigsib/BigSib.java
+++ 10_bigsib/BigSib.java
@@ -1,21 +1,14 @@
 /* Zooming Xippy Zookeepers, Kevin (Mr. Swag), Nicole (Duck), Jomin (Chompsky) 
 APCS
-HW 09 - What are Big Sibs good for?
-2021 10 01
-
+HW 10 - Refactor Big Sib One
+2021 10 04
 DISCOVERIES:
-
-Discovered you can make packages by doing package <name>;
-Package stores file and you can call upon package by doing import <name>.<file>;
 (Must have package <name> in the file that is doing the calling as well)
 Run program by typing javac -d <file>.java
 java <name>.<file>
-
 Unresolved questions:
-
 How to import a specific class of a java file?
 How to import all of the classes of a java file?
-
 */
 
 
