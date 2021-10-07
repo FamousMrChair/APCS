a6f0270e8702e8895f447f2989a5b652106691d3
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-10-07 19:22:13 -0400

    Update Greet.java

--- 14_overload/overcon/Greet.java
+++ 14_overload/overcon/Greet.java
@@ -24,11 +24,11 @@ public class Greet{
                 BigSib tracy = new BigSib("Salutations");
         greeting = richard.greet("freshman");
         System.out.println(greeting);
-                greeting = richard.greet("Dr. Spaceman");
+                greeting = tracy.greet("Dr. Spaceman");
                 System.out.println(greeting);
-                greeting = richard.greet("Kong Fooey");
+                greeting = grizz.greet("Kong Fooey");
                 System.out.println(greeting);
-                greeting = richard.greet("mom");
+                greeting = dotCom.greet("mom");
                 System.out.println(greeting);
     }
 }
