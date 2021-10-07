7baa67d9da1e9a698f5d2440ed44ebed98320eb8
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-10-06 21:55:49 -0400

    Update BigSib.java

--- 13_hatch/BigSib.java
+++ 13_hatch/BigSib.java
@@ -1,12 +1,18 @@
-/* Zooming Xippy Zookeepers, Kevin (Mr. Swag), Nicole (Duck), Jomin (Chompsky) 
+/* Mr. Staley forgot us, Kevin (Mr. Swag) and Corina
 APCS
-HW 12 - On Elder Individuality and the Elimination of Radio Fuzz
-2021 10 05
+HW 13 - Where do BigSibs Come From?
+2021 10 06
 DISCO:
-Using static in "public static void" will create an error message when instance variable HelloMsg is involved.
+Constructors variables can't be changed to other strings once set.
 
 Q/C/C:
-Why is static giving error?
+
+Why do constructors have to be public?
+Why do constructors not have a void element?
+Why do constructors exist?
+Can't we do everything constructors and instance variables do with regular methods?
+Why are we doing this at all?
+What is Corina's ducky's name and will she ever tell me?
 */
 
 public class BigSib {
