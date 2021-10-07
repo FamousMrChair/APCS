9906c3570fdd400b6f97d18f383fdba5dfa586ce
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-10-06 21:18:56 -0400

    Update Greet.java

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -1,7 +1,7 @@
 /* Zooming Xippy Zookeepers, Kevin (Mr. Swag), Nicole (Duck), Jomin (Chompsky) 
 APCS
-HW 12 - On Elder Individuality and the Elimination of Radio Fuzz
-2021 10 05
+HW 13 - Where do BigSibs Come From?
+2021 10 06
 DISCO:
 Using static in "public static void" will create an error message when instance variable HelloMsg is involved.
 
@@ -14,20 +14,17 @@ public class Greet {
     String greeting;
 
     BigSib richard = new BigSib();
-    richard.setHelloMsg("Word up");
-    greeting = richard.greet("freshman");
+
+    greeting = richard.greet("Word up freshman");
+    System.out.println(greeting);
+
+    greeting = richard.greet("Salutations Dr. Spaceman");
     System.out.println(greeting);
 	  
-    richard.setHelloMsg("Salutations");
-    greeting = richard.greet("Dr. Spaceman");
+    greeting = richard.greet("Hey ya Kong Fooey");
     System.out.println(greeting);
 	  
-    richard.setHelloMsg("Hey ya");
-    greeting = richard.greet("Kong Fooey");
-    System.out.println(greeting);
-	  
-    richard.setHelloMsg("Sup");
-    greeting = richard.greet("mom");
+    greeting = richard.greet("Sup mom");
     System.out.println(greeting);
 
   } //end main()
