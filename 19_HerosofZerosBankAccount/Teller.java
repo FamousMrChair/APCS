8ba6e18f9761f3627122bd1fd13fe48ccf221cc8
FamousMrChair <kxiao30@stuy.edu>
2021-10-19 12:30:35 -0400

    setup init

--- 19_HerosofZerosBankAccount/Teller.java
+++ 19_HerosofZerosBankAccount/Teller.java
@@ -1,7 +1,21 @@
+/*
+
+1. Removed ; on line 87 after parameters.
+2. When entered 999999999 and 9999 for the accountnumber and pin, there was no error message
+returned.
+3. 
+*/
+
 public class Teller{
-  BankAccount NIWWD = new BankAccount;
+  
   public static void main(String[] args){
+  BankAccount NIWWD = new BankAccount();
     NIWWD.setPin((short)9999);
-    NIWWD.setAccountNumber(123456789);
-  }
+    NIWWD.setAcctNum(999999999);
+    NIWWD.setPasswd("Hello World!");
+    NIWWD.setName("Mykolyk");
+    NIWWD.setBalance(123214);
+    System.out.println(NIWWD.toString());
+    }
 }
+
