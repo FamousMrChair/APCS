936831d6c2c8c4d5c718c4af391833ad06d6d103
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-10-19 12:17:27 -0400

    Create Teller.java

---
+++ 19_HerosofZerosBankAccount/Teller.java
@@ -0,0 +1,7 @@
+public class Teller{
+  BankAccount NIWWD = new BankAccount;
+  public static void main(String[] args){
+    NIWWD.setPin((short)9999);
+    NIWWD.setAccountNumber(123456789);
+  }
+}
