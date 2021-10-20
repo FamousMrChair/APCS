c28aaf1f4dbf6c70d093832473d78562400f5055
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-10-20 19:09:20 -0400

    Update StatsDriver.java

--- 21_Stats/StatsDriver.java
+++ 21_Stats/StatsDriver.java
@@ -8,6 +8,8 @@ public class StatsDriver {
     System.out.println(Stats.mean(1.0,1.0) + "...should be 1.0");
     System.out.println(Stats.mean(-100,-200) + "...should be -150");
     System.out.println(Stats.mean(-100.2,-200.8) + "...should be -150.6");
+    System.out.println(Stats.mean(-100.0,-200.0,-300.0) + "...should be -200.0");
+    System.out.println(Stats.mean(-100,-200,-300) + "...should be -200");
     System.out.println(Stats.max(1,5) + "... should be 5");
     System.out.println(Stats.max(1.0,5.0) + "...should be 3.0");
     System.out.println(Stats.max(1,10000) + "...should be 10000");
@@ -15,10 +17,10 @@ public class StatsDriver {
     System.out.println(Stats.max(1.123912849,5.123812751) + "...should be 5.123812751");
     System.out.println(Stats.max(-1,-10000) + "...should be -1");
     System.out.println(Stats.geoMean(1,5) + "... should be 5");
-    System.out.println(Stats.geoMean(1.0,5.3) + "... should be 2.3");
+    System.out.println(Stats.geoMean(1.0,9.0) + "... should be 3.0");
     System.out.println(Stats.geoMean(0,0) + "... should be 0");
     System.out.println(Stats.geoMean(0,5903) + "... should be 0");
-    System.out.println(Stats.geoMean(0,0.213124) + "... should be 0");
+    System.out.println(Stats.geoMean(0,0.213124) + "... should be 0.0");
     System.out.println(Stats.geoMean(1,289) + "... should be 17");
   }
 }
