e1031bc36cf62089d206e83e16e2fdbc69da64e6
FamousMrChair <kxiao30@stuy.edu>
2021-10-20 18:16:06 -0400

    setup init

--- 21_Stats/Stats.java
+++ 21_Stats/Stats.java
@@ -1,8 +1,8 @@
 
 /**
-   Clyde "Thluffy" Sinclair
+   Ur mom
    APCS
-   HW20 --
+   HW21 --
    2021-10-21
    xtra: harmonic mean is the reciprocal of the
    arithmetic mean of the reciprocals
@@ -11,34 +11,62 @@
 public class Stats {
 
   public static int mean(int a, int b) {
-
+  	return (a + b)/2;
   }
 
   public static double mean(double a, double b) {
+  	return (a + b)/2.0;
 
   }
 
   public static int max(int a, int b) {
-
+  	if (a > b){
+  		return a;
+  	}
+  	else {
+  		return b;
+  	}
   }
 
   public static double max(double a, double b) {
+   	if (a > b){
+  		return a;
+  	}
+  	else {
+  		return b;
+  	}
 
   }
 
   public static int geoMean(int a, int b) {
-
+  	return Math.sqrt(a * b);
   }
 
   public static double geoMean(double a, double b) {
-
+  	return Math.sqrt(a * b);
   }
 
   public static int max(int a, int b, int c) {
-
+	if (a > b && a > c)){
+  		return a;
+  	}
+  	else if(b > c){
+  		return b;
+  	}
+  	else {
+  		return c;
   }
 
   public static double max(double a, double b, double c) {
+	if (a > b && a > c)){
+  		return a;
+  	}
+  	else if(b > c){
+  		return b;
+  	}
+  	else {
+  		return c;
+  	}
 
   }
 
