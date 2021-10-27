acc8a1b7e5313b804a75cd651b83b67fd387d9a7
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-10-27 19:28:02 -0400

    Update Stats.java

--- 26_gcd/Stats.java
+++ 26_gcd/Stats.java
@@ -13,73 +13,7 @@
 
 
 public class Stats {
-  public static int mean(int a, int b) {
-  	return (a + b)/2;
-  }
-
-  public static double mean(double a, double b) {
-  	return (a + b)/2.0;
-
-  }
-
-  public static int max(int a, int b) {
-  	if (a > b){
-  		return a;
-  	} else {
-  		return b;
-  	}
-  }
-
-  public static double max(double a, double b) {
-   	if (a > b){
-  		return a;
-  	} else {
-  		return b;
-  	}
-
-  }
-
-  public static int geoMean(int a, int b) {
-	int hello = Math.toIntExact(Math.round(Math.sqrt(a * b)));
- 	return hello;
-  }
-
-  public static double geoMean(double a, double b) {
-	double product = (a * b);
-  	return Math.sqrt(product);
-  }
-
-  public static int max(int a, int b, int c) {
-	if (a > b && a > c){
-  		return a;
-  	} else if(b > c){
-  		return b;
-  	} else {
-  		return c;
-	}
-  }
-
-  public static double max(double a, double b, double c) {
-	if (a > b && a > c){
-  		return a;
-  	} else if(b > c){
-  		return b;
-  	} else {
-  		return c;
-  	}
-
-  }
-
-  public static int geoMean(int a, int b, int c) {
-	int hello = Math.toIntExact(Math.round(Math.cbrt(a * b * c)));
- 	return hello;
-  }
-
-  public static double geoMean(double a, double b, double c) {
-	double product = (a * b * c);
-	return Math.cbrt(product);
-
-  }
+ 
   public static int gcd(int a, int b) {
     int counter = 1;
     int g = 1;
@@ -106,11 +40,12 @@ public class Stats {
         return gcdEr(b, (b % a));
      }else{
         return gcdEr(a, (a & b));
+     }
   }
   /*
   public static int gcdEW(int a, int b) {
   }*/
-  public static void main( String[] args ) {
+  public static void main(String[] args) {
     System.out.println("gcd test:");
     System.out.println(gcd(3, 3));
     System.out.println(gcd(3, 6));
