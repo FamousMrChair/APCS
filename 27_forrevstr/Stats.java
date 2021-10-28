0358730a6f067343597ab43b41b4fc714698765f
FamousMrChair <kxiao30@stuy.edu>
2021-10-28 19:35:41 -0400

    setup init

---
+++ 27_forrevstr/Stats.java
@@ -0,0 +1,30 @@
+public class Stats {
+  public static String fenceF(int posts){
+  	String fence = "|";
+  	for (int i = posts; i > 1; i -= 1){
+  		fence += "--|";
+  		}
+  	return fence;
+  }
+  public static String reverseF(String s){
+  	String p = "";
+  	for (int i = String.length(s); i > 0; i -= 1){
+  		p += s.substring(i);
+  		}
+  	return p;
+  }
+  public static void main(String[] args){
+  	System.out.println(fenceF(3));
+  	System.out.println(fenceF(0));
+  	System.out.println(fenceF(4));
+  	System.out.println(fenceF(-4));
+  	System.out.println(fenceF(1));
+  	System.out.println(reverseF("hello there"));
+  	System.out.println(reverseF("tu madred"));
+  	System.out.println(reverseF("23"));
+  	System.out.println(reverseF("fr"));
+  	
+  	
+
+  }
+}
