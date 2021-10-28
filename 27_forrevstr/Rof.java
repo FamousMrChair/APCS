702dac83caf1c766e9086eafaec8f483430e239a
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-10-28 19:55:06 -0400

    Update Rof.java

--- 27_forrevstr/Rof.java
+++ 27_forrevstr/Rof.java
@@ -13,6 +13,11 @@ public class Rof {
   		}
   	return p;
   }
+  public static String reverseR(String s){
+    if (s.length() > 0){
+      return s.substring(s.length()-1,s.length) + reverseR(s.substring(0,s.length()-1));
+    } else{
+      return s;
   public static void main(String[] args){
   	System.out.println(fenceF(3));
   	System.out.println(fenceF(0));
@@ -23,6 +28,10 @@ public class Rof {
   	System.out.println(reverseF("tu madred"));
   	System.out.println(reverseF("23"));
   	System.out.println(reverseF("fr"));
+    System.out.println(reverseR("hello there"));
+    System.out.println(reverseR("tu madred"));
+    System.out.println(reverseR("23"));
+    System.out.println(reverseR("fr"));
   	
   	
 
