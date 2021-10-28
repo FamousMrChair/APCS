59cb648bfca2d0248729454ac2a2d3374b4d5873
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-10-28 19:47:17 -0400

    Update Rof.java

--- 27_forrevstr/Rof.java
+++ 27_forrevstr/Rof.java
@@ -9,7 +9,7 @@ public class Rof {
   public static String reverseF(String s){
   	String p = "";
   	for (int i = s.length(); i > 0; i -= 1){
-  		p += s.substring(i,i-1);
+  		p += s.substring(i-1,i);
   		}
   	return p;
   }
