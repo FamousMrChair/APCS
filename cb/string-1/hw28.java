638579d9b8820cd4f9e2a301ea08a64bdc57a975
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-10-31 14:34:14 -0400

    Update hw28.java

--- cb/string-1/hw28.java
+++ cb/string-1/hw28.java
@@ -7,9 +7,20 @@ helloName("X") → "Hello X!"
   public String helloName(String name) {
     return "Hello " + name + "!";
   }
+/*Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+makeAbba("Hi", "Bye") → "HiByeByeHi"
+makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+makeAbba("What", "Up") → "WhatUpUpWhat"
+*/
+  public String makeAbba(String a, String b) {
+    return a + b + b + a;
+  }
   public static void main(String[] args){
     System.out.println(helloName("Bob"));
     System.out.println(helloName("Alice"));
     System.out.println(helloName("X"));
+    System.out.println(makeabba("Hi", "Bye"));
+    System.out.println(makeabba("Yo", "Alice"));
+    System.out.println(makeabba("What", "Up"));
   }
 }
