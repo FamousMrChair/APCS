0005eb143d61ff1011657a95f45d74873d429c9c
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-10-31 14:35:05 -0400

    Update hw28.java

--- cb/string-1/hw28.java
+++ cb/string-1/hw28.java
@@ -4,7 +4,7 @@ helloName("Bob") → "Hello Bob!"
 helloName("Alice") → "Hello Alice!"
 helloName("X") → "Hello X!"
 */
-  public String helloName(String name) {
+  public static String helloName(String name) {
     return "Hello " + name + "!";
   }
 /*Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
@@ -12,7 +12,7 @@ makeAbba("Hi", "Bye") → "HiByeByeHi"
 makeAbba("Yo", "Alice") → "YoAliceAliceYo"
 makeAbba("What", "Up") → "WhatUpUpWhat"
 */
-  public String makeAbba(String a, String b) {
+  public static String makeAbba(String a, String b) {
     return a + b + b + a;
   }
   public static void main(String[] args){
