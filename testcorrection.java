f4249e2cb0f773d10b0494000cda1f2da07c811c
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-11-10 20:38:46 -0500

    Update testcorrection.java

--- testcorrection.java
+++ testcorrection.java
@@ -1,7 +1,7 @@
 public class testcorrection {
   double x = 5;
   double y = 2;
-  public static int hello(int m){
+  public int hello(int m){
     int m = (int)(x+y+x/y - x * y -x / (10*y));
     System.out.println(m);
     return m;
