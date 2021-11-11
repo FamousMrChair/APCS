8ae61cd76641515365e749b12b2e5612ab240d9a
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-11-10 20:34:55 -0500

    Create testcorrection.java

---
+++ testcorrection.java
@@ -0,0 +1,8 @@
+public class testcorrection {
+  double x = 5;
+  double y = 2;
+  public static void main(String[] args){
+    int m = (int)(x+y+x/y - x * y -x / (10*y));
+    System.out.println(m);
+  }
+}
