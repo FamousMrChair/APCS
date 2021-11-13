5e975fce2b9033d18d0168c606c59caef77d5be0
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-11-12 22:43:50 -0500

    Update Loopier.java

--- 34_array/Loopier.java
+++ 34_array/Loopier.java
@@ -61,7 +61,7 @@ public class Loopier {
       }
     if (a[a.length-1] == target) {
       return freqR(array, target) + 1;
-    } else if (a[a.lentgh-1] != target && a.length >1){
+    } else if (a[a.length-1] != target && a.length >1){
       return freqR(array,target);
     } else {
       return 0;
