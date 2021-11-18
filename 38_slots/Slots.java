f65c16034b9cfbb85acf00074eeb39f1c8ba2a7e
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2021-11-17 21:28:24 -0500

    Update Slots.java

--- 38_slots/Slots.java
+++ 38_slots/Slots.java
@@ -37,7 +37,7 @@ public class Slots {
   public Slots()
   {
     //allocate memory for _fruits based on size of FRUITS:
-    _fruits = new String[_fruits.length];
+    _fruits = new String[FRUITS.length];
 
     //copy elements of FRUITS into _fruits:
     for (int i = 0; i < FRUITS.length; i++) {
