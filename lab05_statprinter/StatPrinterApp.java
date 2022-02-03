475a3421a04130d57b190f7134b1602f998855f0
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2022-02-03 18:17:06 -0500

    Update StatPrinterApp.java

--- lab05_statprinter/StatPrinterApp.java
+++ lab05_statprinter/StatPrinterApp.java
@@ -38,11 +38,12 @@ public class StatPrinterApp
 
     StatPrinter sp1 = new StatPrinter( al1 );
 
-    /*
       System.out.println( "sp1 testing... " );
       sp1.max( al1 );
+    System.out.println(sp1.get(2));
+    /*
       sp1.printHistogram( 50 );
-    */
+    
 
 
     //construct data [2,3,2,5,2,3]
@@ -65,6 +66,7 @@ public class StatPrinterApp
     }
     System.out.println( "histogram:" );
     sp2.printHistogram( 50 );
+    */
   }
 
 }//end StatPrinterApp
