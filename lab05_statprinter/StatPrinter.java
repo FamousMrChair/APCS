72129e4e582d21c80fbdcb7fa0770942a68bc188
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2022-02-04 15:14:19 -0500

    Update StatPrinter.java

--- lab05_statprinter/StatPrinter.java
+++ lab05_statprinter/StatPrinter.java
@@ -7,9 +7,7 @@
 
 /**
    An AP-style question, for practice...
-
    GOAL: Write the StatPrinter class below.
-
    WHAT YOU NEED TO KNOW:
    The StatPrinter Object receives an
    ArrayList of nonnegative integers, then builds a frequency ArrayList in which 
@@ -18,19 +16,16 @@
    list would be [0,1,2,2,0,1]. This is read as 0 zeroes, 1 one, 2 twos, 
    2 threes, 0 fours, 1 five. The size of the frequency list is the equal to 
    the maximum value of the data.
-
    A capability of the class is to calculate local modes from the frequency 
    list. A local mode is a value that is greater than the value at index-1 
    and greater than the value at index+1. A local mode is never at the end 
    points of the list. For example, if the frequency list is [1,2,1,4,2,3,5] 
    then the local modes are 2 and 4.
-
    This class is also capable of printing a histogram of the frequencies, using 
    '*'s to indicate a frequency amount. To print a histogram, the user specifies 
    the longest sequence of '*'s used and then all other values are printed in 
    proportion to this value. For example, if longest bar is 10 and the frequency 
    list is [1,2,1,4,2,3,5] then the histogram printed looks like this:
-
    0 : **
    1 : ****
    2 : **
@@ -38,9 +33,7 @@
    4 : ****
    5 : ******
    6 : **********
-
    For each method, state run time efficiency using Big O notation.
-
    TIPS FOR AWESOME:
    * Keys to Success were so named (by Thinkeren) for a reason.
    * Look over all fxns, think a bit, decide which to tackle first.
@@ -71,6 +64,7 @@ public class StatPrinter
     }
     return max;
   }
+  // Big O notation is O(n)
    
   //*************** QUESTION 02 **************************
   //precond:  data.size() > 0, each entry b/t 0,100 inclusive
@@ -93,10 +87,7 @@ public class StatPrinter
      }
    
   }
-
-
-
-
+  // Big O notation is O(n^2)
 
   //*************** QUESTION 03 **************************
   //postcond: returns true if i > 0 and i < _frequency.size() - 1
@@ -118,7 +109,7 @@ public class StatPrinter
    return false;
   
   }
-
+  // Big O notation is O(1)
 
   //*************** QUESTION 04 **************************
   //postcond: returns list of modes in _frequency
@@ -132,7 +123,7 @@ public class StatPrinter
      }
      return modes;
   }
-
+  // Big O notation is O(1)
 
   //*************** QUESTION 05 **************************
   //precond:  longestBar > 0
@@ -151,8 +142,8 @@ public class StatPrinter
          System.out.print("\n");
       }
   }
+   // Big O notation is O(n^2)
   
- 
    public String toString(){
       return _frequency.toString();
    }
