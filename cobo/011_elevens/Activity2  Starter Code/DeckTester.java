60e40b5f9ddc0736830f5d59c1677659f9ef5587
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2022-03-20 16:41:37 -0400

    Update DeckTester.java

--- cobo/011_elevens/Activity2  Starter Code/DeckTester.java
+++ cobo/011_elevens/Activity2  Starter Code/DeckTester.java
@@ -9,14 +9,21 @@ public class DeckTester {
 	 */
 	public static void main(String[] args) {
 		String[] ranks = new String[3];
-		ranks[1] = "one";
-		ranks[2] = "two";
-		ranks[3] = "three";
-		Deck Kevin1 = new Deck(ranks, ["spade","diamond"], [1,2,3]);
+		ranks[0] = "one";
+		ranks[1] = "two";
+		ranks[2] = "three";
+		String[] suits = new String[2];
+		suits[0] = "spade";
+		suits[1] = "diamond";
+		Integer[] values = new Integer[3];
+		values[0] = 1;
+		values[1] = 2;
+		values[2] = 3;
+		Deck Kevin1 = new Deck(ranks, suits, values);
 		System.out.println(Kevin1.size());
-		Deck Kevin2 = new Deck(["four", "five", "six"], ["heart","clover"], [4,5,6]);
+		Deck Kevin2 = new Deck(ranks, suits, values);
 		System.out.println(Kevin2.deal());
-		Deck Kevin3 = new Deck(["seven", "eight", "nine"], ["spade","heart"], [7,8,9]);
+		Deck Kevin3 = new Deck(ranks, suits, values);
 		System.out.println(Kevin3);		
 
 		
