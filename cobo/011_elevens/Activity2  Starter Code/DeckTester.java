b80ce487c2cdf8c683b980388795566e81a9377d
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2022-03-20 16:33:09 -0400

    Update DeckTester.java

--- cobo/011_elevens/Activity2  Starter Code/DeckTester.java
+++ cobo/011_elevens/Activity2  Starter Code/DeckTester.java
@@ -8,12 +8,9 @@ public class DeckTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		Deck Kevin1 = new Deck(['one', 'two', 'three'], ['spade','diamond'], [1,2,3]);
+		Deck Kevin1 = new Deck('one', 'two', 'three', 'spade','diamond', 1,2,3);
 		System.out.println(Kevin1.size());
-		Deck Kevin2 = new Deck(['four', 'five', 'six'], ['heart','clover'], [4,5,6]);
-		System.out.println(Kevin2.deal());
-		Deck Kevin3 = new Deck(['seven', 'eight', 'nine'], ['spade','heart'], [7,8,9]);
-		System.out.println(Kevin3);
+
 		
 	}
 }
