523a2fb36d04604f590b2ca621c3ac3e39103e83
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2022-03-22 13:49:39 -0400

    Update List.java

--- 79_dllist/List.java
+++ 79_dllist/List.java
@@ -2,33 +2,34 @@
  * interface List
  * subset of actual Java List interface
  * Specifies actions a List must be able to perform.
- * 
+ *
  * If placed in current working dir, will be consulted instead of
  *  built-in Java library interface.
- * 
+ *
  * new in version 2.0: 
  * REMOVE-AT-INDEX
  * ADD-AT-INDEX
  **/
 
-public interface List<T>
+public interface List
 {
   //add node to list, containing input String as its data
   public boolean add( String x );
 
   //insert at specified index
-  public void add( int index, T newVal ); 
+  public void add( int index, String newVal );
 
   //remove node from list, return its data
-  public T remove( int index );
+  public String remove( int index );
 
   //return data in element at position i
-  public T get( int i );
+  public String get( int i );
 
   //overwrite data in element at position i
-  public T set( int i, T x );
+  public String set( int i, String x );
 
   //return length of list
   public int size();
 
 }//end interface
+
