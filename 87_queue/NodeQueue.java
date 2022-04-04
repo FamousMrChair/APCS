5d14e923a574cafa078c004dfa0f3bd4148c2799
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2022-04-03 21:45:45 -0400

    Update NodeQueue.java

--- 87_queue/NodeQueue.java
+++ 87_queue/NodeQueue.java
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-public class NodeQueue<QUASAR> implements Queue<QUASAR>{
-
-  private QUASAR _cargo;    //cargo may only be of type String
-  private LLNode _nextNode; //pointers to next, prev DLLNodes
-  private LLNode _head;
-  private int _size;
-
-  public NodeQueue(QUASAR value, LLNode next){
-    _cargo = value;
-    _size = 0;
-    _nextNode = next;
-
-  }
-
-  public QUASAR getCargo() { return _cargo; }
-
-  public LLNode getNext() { return _nextNode; }
-
-  public QUASAR setCargo( QUASAR newCargo )
-  {
-    QUASAR foo = getCargo();
-    _cargo = newCargo;
-    return foo;
-  }
-
-  public DLLNode setNext( DLLNode newNext )
-  {
-    DLLNode foo = getNext();
-    _nextNode = newNext;
-    return foo;
-  }
-
-  public QUASAR dequeue(){
-    _head.getCdr =
-  }
-}
-
-
-
-
-
-
-  // constructor -- initializes instance vars
-
-
-  //--------------v  ACCESSORS  v--------------
-
-=======
 /*
 Team Three Kevins: Hamim Seam, Kevin Li, Kevin Xiao
 APCS
@@ -191,21 +142,10 @@ public class NodeQueue<QUASAR> implements Queue<QUASAR>
   {
     return _nextNode;
   }
->>>>>>> 7255e6839ced33462049e685bc9686c85054918f
   //--------------^  ACCESSORS  ^--------------
 
 
   //--------------v  MUTATORS  v--------------
-<<<<<<< HEAD
-
-
-
-
-  public DLLNode setPrev( DLLNode newPrev )
-  {
-    DLLNode foo = getPrev();
-    _prevNode = newPrev;
-=======
   public QUASAR setCargo( QUASAR newCargo )
   {
     QUASAR foo = getCargo();
@@ -217,39 +157,11 @@ public class NodeQueue<QUASAR> implements Queue<QUASAR>
   {
     LLNode<QUASAR> foo = getNext();
     _nextNode = newNext;
->>>>>>> 7255e6839ced33462049e685bc9686c85054918f
     return foo;
   }
   //--------------^  MUTATORS  ^--------------
 
 
-<<<<<<< HEAD
-  // override inherited toString
-  public String toString() { return _cargo.toString(); }
-
-
-  //main method for testing
-  public static void main( String[] args )
-  {
-    //Below is an exercise in creating a linked list...
-
- /*********************
-    //Create a node
-    DLLNode first = new DLLNode( "cat", null );
-    //Create a new node after the first
-    first.setNext( new DLLNode( "dog", null ) );
-    //Create a third node after the second
-    first.getNext().setNext( new DLLNode( "cow", null ) );
-    DLLNode temp = first;
-    while( temp != null ) {
-      System.out.println( temp );
-      temp = temp.getNext();
-    }
-   ***********************/
-  }//end main
-
-}//end class DLLNode
-=======
   // override inherited toQUASAR
   public String toString()
   {
@@ -259,4 +171,3 @@ public class NodeQueue<QUASAR> implements Queue<QUASAR>
   }
 
 }
->>>>>>> 7255e6839ced33462049e685bc9686c85054918f
