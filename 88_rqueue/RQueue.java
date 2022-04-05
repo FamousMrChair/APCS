9044a2e3af037763c5d71f4bc433662de2416afa
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2022-04-04 22:22:56 -0400

    Update RQueue.java

--- 88_rqueue/RQueue.java
+++ 88_rqueue/RQueue.java
@@ -11,7 +11,18 @@
  *  linkages point opposite direction for O(1) en/dequeuing
  *            N <- N <- ... <- N <- N
  *      _end -^                     ^- _front
- *
+ * 
+Team Three Kevins: Kevin Li, Hamim Seam, Kevin Xiao
+APCS
+HW88 -- Queue with random dequeuing
+2022-04-04
+time spent: 1 hour
+DISCO
+    - We can randomize the dequeueing by just shuffling the list when we add 
+QCC
+    - the sample() method feels unoptimized
+    - How "shuffled" does the queue have to be?
+    - How is FIIDKO analogous to real life "queues"?
  **/
 
 
@@ -25,51 +36,104 @@ public class RQueue<SWASHBUCKLE> implements Queue<SWASHBUCKLE>
   // default constructor creates an empty queue
   public RQueue()
   {
-
+    _front = null;
+    _end = null;
+    _size = 0;
   }
 
 
-  public void enqueue( T enQVal )
+  public void enqueue( SWASHBUCKLE enQVal )
   {
+    if (isEmpty()) {
+        _front = new LLNode<SWASHBUCKLE>(enQVal, null);
+        _end = _front;
+        _size++;
+        return;
+    }
 
-  }//O(?)
+    _end.setNext(new LLNode<SWASHBUCKLE>(enQVal, null));
+    _end = _end.getNext();
+    _size++;
+    sample();
+
+  }//O(n)
 
 
   // remove and return thing at front of queue
   // assume _queue ! empty
-  public T dequeue()
+  public SWASHBUCKLE dequeue()
   {
+    SWASHBUCKLE output = _front.getCargo();
+    _front = _front.getNext();
+    _size--;
+    return output;
+  }//O(1)
 
-  }//O(?)
 
-
-  public T peekFront()
+  public SWASHBUCKLE peekFront()
   {
-
-  }//O(?)
+    return _front.getCargo();
+  }//O(1)
 
 
   /***
    * void sample() -- a means of "shuffling" the queue
    * Algo:
-   *   < YOUR SUCCINCT SUMMARY HERE >
+   *   1. Place each node into an array
+   *   2. Swap each node with a node at a random index
+   *   3. Reconstruct the queue with the nodes in the same order as in the array
+   *   4. Update the _front and _end pointers
    **/
   public void sample ()
   {
+    LLNode<SWASHBUCKLE>[] nodes = new LLNode[_size];
+    LLNode<SWASHBUCKLE> temp = _front;
+    LLNode<SWASHBUCKLE> swapTemp;
+    int swapIndex;
+    
+    //place the nodes into an array
+    for (int i = 0; i < _size; i++) {
+        nodes[i] = temp;
+        temp = temp.getNext();
+    }
 
-  }//O(?)
+    //swap nodes to randomize order of nodes in array
+    for (int i = 0; i < _size; i++) {
+        swapIndex = (int)(Math.random() * _size);
+        swapTemp = nodes[i];
+        nodes[i] = nodes[swapIndex];
+        nodes[swapIndex] = swapTemp;
+    }
 
+    //reconstruct the queue
+    for (int i = 0; i < _size - 1; i++) {
+        nodes[i].setNext(nodes[i+1]);
+    }
+    //make the last element point to null
+    nodes[_size - 1].setNext(null);
+
+    //update pointers
+    _front = nodes[0];
+    _end = nodes[_size - 1];
+
+  }//O(n)
 
   public boolean isEmpty()
   {
     return _front == null;
-  } //O(?)
+  } //O(1)
 
 
   // print each node, separated by spaces
   public String toString()
   {
-
+    String output = "<- front ";
+    LLNode<SWASHBUCKLE> tmp = _front;
+    for (int i = 0; i < _size; i++) {
+        output = tmp.getCargo() + " " + output;
+        tmp = tmp.getNext();
+    }
+    return output;
   }//end toString()
 
 
@@ -77,10 +141,9 @@ public class RQueue<SWASHBUCKLE> implements Queue<SWASHBUCKLE>
   //main method for testing
   public static void main( String[] args )
   {
-
-      /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
     
     Queue<String> PirateQueue = new RQueue<String>();
+
     System.out.println("\nnow enqueuing..."); 
     PirateQueue.enqueue("Dread");
     PirateQueue.enqueue("Pirate");
@@ -88,18 +151,37 @@ public class RQueue<SWASHBUCKLE> implements Queue<SWASHBUCKLE>
     PirateQueue.enqueue("Blackbeard");
     PirateQueue.enqueue("Peter");
     PirateQueue.enqueue("Stuyvesant");
+
     System.out.println("\nnow testing toString()..."); 
     System.out.println( PirateQueue ); //for testing toString()...
+
     System.out.println("\nnow dequeuing..."); 
-    System.out.println( PirateQueue.dequeue() );
-    System.out.println( PirateQueue.dequeue() );
-    System.out.println( PirateQueue.dequeue() );
-    System.out.println( PirateQueue.dequeue() );
-    System.out.println( PirateQueue.dequeue() );
-    System.out.println( PirateQueue.dequeue() );
+    System.out.println( "predicted: " + PirateQueue.peekFront() + " \n " + PirateQueue.dequeue() );
+    System.out.println( "predicted: " + PirateQueue.peekFront() + " \n " + PirateQueue.dequeue() );
+
+    System.out.println( "\n" + PirateQueue ); //for testing toString()...
+    System.out.println( "enqueuing Sparrow: ");
+    PirateQueue.enqueue("Sparrow");
+    System.out.println(PirateQueue + "\n");
+
+    System.out.println( "predicted: " + PirateQueue.peekFront() + " \n " + PirateQueue.dequeue() );
+    System.out.println( "predicted: " + PirateQueue.peekFront() + " \n " + PirateQueue.dequeue() );
+
+    System.out.println( "\n" + PirateQueue ); //for testing toString()...
+    System.out.println( "enqueuing Jack: ");
+    PirateQueue.enqueue("Jack");
+    System.out.println(PirateQueue + "\n");
+
+    System.out.println( "predicted: " + PirateQueue.peekFront() + " \n " + PirateQueue.dequeue() );
+    System.out.println( "predicted: " + PirateQueue.peekFront() + " \n " + PirateQueue.dequeue() );
+
+    System.out.println( "\n" + PirateQueue );
+
+    /*
     System.out.println("\nnow dequeuing fr empty queue...\n" +
                        "(expect NPE)\n"); 
     System.out.println( PirateQueue.dequeue() );
+    /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
       ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
 
   }//end main
