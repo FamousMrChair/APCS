080c314d100277835b27f312916108407b04ecb5
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2022-04-12 21:05:51 -0400

    Update ALDeque.java

--- 90_dequeue/ALDeque.java
+++ 90_dequeue/ALDeque.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class LLDeque<T> implements Deque<T>{
+public class ALDeque<T> implements Deque<T>{
   ArrayList deque<T> = new ArrayList<T>();
   
   public void addFirst( T element) {
@@ -24,17 +24,31 @@ public class LLDeque<T> implements Deque<T>{
   }
     
   public T peekFirst() {
-    return deque.get(0);
+    if (!deque.isEmpty()) {
+      return deque.get(0);
+    }
+    return null;
   }
   
   public T peekLast() {
-    return deque.get(deque.size - 1);
+    if (!deque.isEmpty()) {
+      return deque.get(deque.size() - 1);
+    }
+    return null;
   }
   
   public int size() {
     return deque.size();
   }
-	
+  
+  public boolean offerFirst( T element ) {
+		
+  }
+  
+  public boolean offerLast( T element ) {
+    
+  }
+
   public T pollFirst() {
 		if (!deque.isEmpty()) {
       return removeFirst();
@@ -49,3 +63,29 @@ public class LLDeque<T> implements Deque<T>{
     return null;
   }
   
+  public T getFirst() {
+    return deque.get(0);
+	}
+  
+  public T getLast() {
+    return deque.get(deque.size() - 1);
+  }
+  
+  public static void main (String[] args){
+    Deque<T> Kevin = new ALDeque<T>();
+    for (int i = 0; i < 50; i ++){
+      Kevin.addFirst(i);
+    }
+    for (int i = 0; i < 50; i ++){
+      Kevin.addLast(i);
+    }
+    while (deque.size() > 0){
+      if (deque.size() > 0) {
+        System.out.println(Kevin.removeFirst());
+      }
+      if (deque.size() > 0) {
+        System.out.println(Kevin.removeLast());
+      }
+    }
+  }
+}
